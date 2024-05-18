e5a20a75c4111bd109700e7551f143f9bc1363e4
Allen Zheng <zhenga8533@gmail.com>
2024-05-17 23:49:07 -0400

    changed npc to save to dict

--- npc_scraper.py
+++ npc_scraper.py
@@ -4,26 +4,40 @@ from core import get_html, save_html
 
 NPC_URL = 'https://wiki.hypixel.net/NPCs'
 
-def get_npcs(html: str) -> None:
+def get_npcs(html: str) -> dict:
+    """
+    Get the NPCs from the HTML.
+
+    :param html: The HTML to get the NPCs from.
+    :return: The NPCs from the HTML.
+    """
+
     soup = BeautifulSoup(html, 'html.parser')
     blocks = soup.find_all(['blockquote', 'h4'])
+    npcs = {}
 
     last_area = 'Unknown'
     for block in blocks:
+        # Find the area name
         if block.name == 'blockquote':
             area_tag = block.find('a', title=True)
             last_area = area_tag['title'] if area_tag else 'Unknown'
+
+        # Create the area if it doesn't exist
+        if last_area not in npcs:
+            npcs[last_area] = []
         
-        if block.name == 'h4' or block.find_next_sibling('table', class_='wikitable mw-collapsible'):
-            table = block.find_next_sibling('table', class_='wikitable mw-collapsible')
-            if table:
-                rows = table.find_all('tr')
-                for row in rows:
-                    title_tag = row.find('a', title=True)
-                    if title_tag:
-                        print(f"NPC: {title_tag['title']}, Area: {last_area}")
+        # Find the NPCs
+        table = block.find_next_sibling('table', class_='wikitable mw-collapsible')
+        if table:
+            rows = table.find_all('tr')
+            for row in rows:
+                title_tag = row.find('a', title=True)
+                if title_tag:
+                    npcs[last_area].append(title_tag['title'])
+    
+    return npcs
 
 if __name__ == '__main__':
     html = get_html(NPC_URL)
-    save_html(html, 'html/npcs.html')
-    get_npcs(html)
+    print(get_npcs(html))
