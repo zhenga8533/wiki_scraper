492c4d0225f0e55f845c2b4ee5bd27ea27f46655
Allen Zheng <zhenga8533@gmail.com>
2024-05-18 20:51:03 -0400

    completed Fairy Soul scraper

--- src/constants.py
+++ src/constants.py
@@ -1,3 +1,4 @@
 BASE_URL = 'https://wiki.hypixel.net'
+FAIRY_SOUL_URL = f'{BASE_URL}/Fairy_Souls'
 NPC_URL = f'{BASE_URL}/NPCs'
 LOCATION_URL = f'{BASE_URL}/Locations'
---
+++ src/fairy_scraper.py
@@ -0,0 +1,51 @@
+from bs4 import BeautifulSoup
+from core import *
+from constants import *
+
+
+def get_fairy_souls(html: str) -> dict:
+    """
+    Get the fairy souls from the HTML.
+
+    :param html: The HTML to get the fairy souls from.
+    :return: The fairy souls from the HTML.
+    """
+
+    fairy_souls = {}
+    soup = BeautifulSoup(html, 'html.parser')
+    divs = soup.find_all('div', class_='hp-tabcontent')
+
+    for div in divs:
+        # Find wiki table in the div
+        table = div.find('table', class_='wikitable')
+        if table is None:
+            continue
+
+        # Find the rows in the table
+        rows = table.find_all('tr')[1:]
+        div_id = div.get('id').rstrip('_')
+        souls = []
+
+        for row in rows:
+            # Find the columns in the row
+            cols = row.find_all('td')
+            if len(cols) < 4:
+                continue
+
+            # Extract the fairy soul data from the columns
+            x = cols[0].get_text(strip=True)
+            y = cols[1].get_text(strip=True)
+            z = cols[2].get_text(strip=True)
+            zone = cols[3].get_text(strip=True)
+            souls.append([zone, x, y, z])
+
+        if len(souls) > 0:
+            fairy_souls[div_id] = souls
+
+    return fairy_souls
+
+if __name__ == '__main__':
+    html = get_html(FAIRY_SOUL_URL)
+    save_html(html, 'fairy_souls.html')
+    fairy_souls = get_fairy_souls(html)
+    save_json(fairy_souls, 'fairy_souls.json')
