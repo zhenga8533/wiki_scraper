ccfae808e7617a0e65b125d9f2bc2f2b3ba083f4
Allen Zheng <zhenga8533@gmail.com>
2024-07-18 13:52:41 -0400

    Create directory if DNE

--- src/core.py
+++ src/core.py
@@ -1,18 +1,20 @@
 import bs4
 import json
+import os
 import requests
 
 
 def get_html(url):
     """
     Get the HTML from a URL.
-    
+
     :param url: The URL to get the HTML from.
     :return: The HTML from the URL.
     """
 
     response = requests.get(url)
     return response.text
+
 
 def save_html(html: str, file_name: str):
     """
@@ -22,12 +24,14 @@ def save_html(html: str, file_name: str):
     :param file_name: The path to save the HTML to.
     """
 
-    soup = bs4.BeautifulSoup(html, 'html.parser')
+    soup = bs4.BeautifulSoup(html, "html.parser")
     pretty_html = soup.prettify()
 
-    with open(f'html/{file_name}', 'w', encoding='utf-8') as file:
+    os.makedirs("html", exist_ok=True)
+    with open(f"html/{file_name}", "w", encoding="utf-8") as file:
         file.write(pretty_html)
-    
+
+
 def save_json(data: dict, file_name: str):
     """
     Save JSON to a file.
@@ -36,5 +40,6 @@ def save_json(data: dict, file_name: str):
     :param file_name: The path to save the JSON to.
     """
 
-    with open(f'json/{file_name}', 'w') as file:
+    os.makedirs("json", exist_ok=True)
+    with open(f"json/{file_name}", "w") as file:
         json.dump(data, file, indent=4)
