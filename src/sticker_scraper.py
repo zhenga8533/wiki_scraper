fcd3b90994a37ff043682f4d14d1166dbe13579e
Allen Zheng <zhenga8533@gmail.com>
2024-07-18 15:51:01 -0400

    Completed sticker scraper

--- src/sticker_scraper.py
+++ src/sticker_scraper.py
@@ -3,7 +3,7 @@ from core import *
 from constants import STICKER_URL
 
 
-def get_stickers(html: str) -> dict:
+def get_stickers(html: str) -> list:
     """
     Get the stickers from the HTML.
 
@@ -12,13 +12,30 @@ def get_stickers(html: str) -> dict:
     """
 
     soup = BeautifulSoup(html, "html.parser")
-    stickers = {}
+    stickers = []
+
+    tables = soup.find_all("table", class_="article-table mw-collapsible mw-collapsed article-table striped sortable")
+    for table in tables:
+        headers = ["Image URL", "Name", "Description", "Stack Boost", "Stack Reward", "Where it's from"]
+        rows = table.find_all("tr")[1:]  # Skip the header row
+
+        for row in rows:
+            cells = row.find_all(["th", "td"])
+            sticker_data = {
+                "image_url": cells[0].find("a")["href"],
+                "name": cells[1].text.strip(),
+                "description": cells[2].text.strip(),
+                "stack_boost": cells[3].text.strip(),
+                "stack_reward": cells[4].text.strip(),
+                "where_from": cells[5].text.strip(),
+            }
+            stickers.append(sticker_data)
 
     return stickers
 
 
 if __name__ == "__main__":
     html = get_html(STICKER_URL)
-    save_html(html, "sticker.html")
+    # save_html(html, "sticker.html")
     stickers = get_stickers(html)
     save_json(stickers, "stickers.json")
